c2eb73679fcca955e7f9770afa0f0b3cdd68d3ef
rakshakh10223 <82376694+rakshakh10223@users.noreply.github.com>
2021-06-18 18:32:56 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,50 @@
+
+Canvas
+ block_y=1;
+ block_x=1;
+
+block_image_width = 350;
+block_image_height = 430;
+
+var block_image_object= "";
+
+function new_image(get_image)
+{
+	("blackg.jpg")
+}
+
+window.addEventListener("keydown", my_keydown);
+
+function my_keydown(e)
+{
+keyPressed = e.keyCode;
+console.log(keyPressed);
+
+	if(keyPressed == '82') 
+	{
+		"rr1.png"
+	}
+	if(keyPressed == '71')
+	{
+		block_x = 200;
+		"gr.png"
+	}
+	
+	if(keyPressed == '89')
+	{
+		block_x =350;
+		"yr.png"
+	}
+	if(keyPressed == '80')
+	{
+		block_x = 600;
+		"pr.png"
+	}
+	if(keyPressed == '66')
+	{
+		block_x = 700;
+	'br.png'
+	}
+	
+}
+
